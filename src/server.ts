92354bd4b001d4ad2aad2ef01fae88a48cf05855
leoprietsch <leo.prietsch@gmail.com>
2022-02-01 09:42:03 -0300

    feat: add json parser middleware

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,10 @@ import express from 'express';
 const app = express();
 const port = process.env.port || 5000;
 
-app.get('/', (request, response) => {
-  response.send('Initial setup');
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Initial setup');
 });
 
 app.listen(port);
